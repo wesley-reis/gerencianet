6e314e899aedbb635d968dbfe1a4a8ad2f937fa5
Wesley Rodrigues dos Reis <wesleyepolly16@gmail.com>
2022-06-26 19:06:52 -0300

    criando barra de prograsso e manageBeanaAbstract

---
+++ src/main/java/br/com/project/bean/geral/BeanManagedViewAbstract.java
@@ -0,0 +1,14 @@
+package br.com.project.bean.geral;
+
+import org.springframework.stereotype.Component;
+
+import br.com.project.report.util.BeanReportyView;
+
+@Component
+public abstract class BeanManagedViewAbstract extends BeanReportyView {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
