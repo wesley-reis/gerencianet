f1294da2837cc224192c1a3f60a997ef7492ea1e
Wesley Rodrigues dos Reis <wesleyepolly16@gmail.com>
2022-06-26 17:18:05 -0300

    interfaces de Service login

---
+++ src/main/java/br/com/srv/interfaces/SrvLogin.java
@@ -0,0 +1,11 @@
+package br.com.srv.interfaces;
+
+import java.io.Serializable;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface SrvLogin extends Serializable{
+	
+	boolean autentico(String login, String senha) throws Exception;
+}
